cc21a86eb5271a47c4096d2910bec439af0795a3
antonbakker <Anton@Antons-iMac2015.local>
2017-06-09 13:02:33 +0200

    Aanpassingen

--- Projectenoverzicht.sql
+++ Projectenoverzicht.sql
@@ -4,7 +4,8 @@ DECLARE @name VARCHAR(50) -- database name
 DECLARE db_cursor CURSOR FOR 
 SELECT name
 FROM master.dbo.sysdatabases
-WHERE name IN ('512', '513', '514', '521', '522', '523', '524', '525', '527', '528', '541', '542', '543', '544', '561', '571', '581', '582', '583', '591', '593', '594')  -- include these databases
+WHERE name IN ('521')  -- include these databases
+/* WHERE name IN ('512', '513', '514', '521', '522', '523', '524', '525', '527', '528', '541', '542', '543', '544', '561', '571', '581', '582', '583', '591', '593', '594') */  -- include these databases
 /* WHERE name IN ('012', '013', '014', '021', '022', '023', '024', '025', '027', '028', '041', '042', '043', '044', '061', '071', '081', '082', '083', '091', '093', '094') */  -- include these databases
 /* WHERE name IN ('521') */
  
@@ -21,22 +22,16 @@ DECLARE @query VARCHAR(MAX)
 SET @query =
 '
     SELECT
+        PRJ.Division AS Divisie,
         PRJ.ProjectNr AS Projectnummer,
         PRJ.Description AS Projectomschrijving,
-        PRJ.Division AS Divisie,
         DIV.bedrnm AS Werkmaatschappij,
-        -- PRJ.Memo,
-        -- PRJ.NumberField1,
-        -- PRJ.NumberField2,
-        -- PRJ.NumberField3,
-        -- PRJ.NumberField4,
-        -- PRJ.NumberField5,
         PRJ.TextField1 AS Projecleider,
         PRJ.TextField2 AS Uitvoerder,
         PRJ.TextField3 AS Calculator,
         PRJ.TextField4 AS Vrijveld,
         PRJ.TextField5 AS Kernactiviteit,
-        CIC.[cmp_code] AS Relatiecode,
+        -- CIC.[cmp_code] AS Relatienummer,
         CIC.[cmp_name] AS Relatienaam,
         CIC.[cmp_fadd1] AS Adres,
         CIC.[cmp_fpc] AS Postcode,
